620239559f137b3586fb5f65df99c7af8bb9025b
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-07-25 12:28:39 -0700

    react-loopback config'd

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import App from './App';
 import './index.css';
+import {polyfill} from 'es6-promise';
+polyfill();
+import 'whatwg-fetch';
+
+//Loopback config
+import { config } from 'react-loopback';
+config.set('baseUrl', 'http://localhost:4000/api/');
+config.set('access_token', 'I_AM_AN_ACCESS_TOKEN');
 
 ReactDOM.render(
   <App />,
