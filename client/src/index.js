584d2107c0a33fd5e8c5b8092ba0a2dc735bfab8
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-07-25 10:43:04 -0700

    added es6-promise, whatwg-fetch and react-loopback

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {polyfill} from 'es6-promise';
+polyfill();
+import 'whatwg-fetch';
 
 ReactDOM.render(
   <App />,
