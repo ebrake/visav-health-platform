cf8042a5d7e3ab3a7698d7bb0b6bd435b378cdf1
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-07-26 10:37:38 -0700

    fix server message

--- server/server.js
+++ server/server.js
@@ -45,8 +45,8 @@ class StartServer {
       dataSource.isActual(appModels, function(err, actual) {
         //DataSource.isActual() is false if database structure is outdated WRT model files (model-config.json)
         if (!actual) {
-          console.log('Postgres models used by app:\n' + JSON.stringify(appModels) );
           console.log('Database structure update is necessary... commencing autoupdate.')
+          console.log('Postgres models used by app:\n' + JSON.stringify(appModels) );
           dataSource.autoupdate(appModels, function(err, result) {
             if (err) throw err;
             else console.log('Database structure update complete');
