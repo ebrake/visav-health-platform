b3311957736ec3b117f058e5a5aad7879bb99c36
Nolan Thorn <nolan.thorn@krisandbrake.com>
2016-07-26 11:39:02 -0700

    Restructured project, added "alt" to package, ready to set up stores. needs react-router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,6 @@
+//dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 import {polyfill} from 'es6-promise';
 polyfill();
 import 'whatwg-fetch';
@@ -10,6 +9,10 @@ import 'whatwg-fetch';
 import { config } from 'react-loopback';
 config.set('baseUrl', 'http://localhost:4000/api/');
 config.set('access_token', 'I_AM_AN_ACCESS_TOKEN');
+
+//ours
+import App from './components/App';
+import './css/index.css';
 
 ReactDOM.render(
   <App />,
