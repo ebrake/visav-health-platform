2a07159d396b794b5543b84e93a2d1cb8b8fe0c4
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-07-27 14:12:55 -0700

    cleanup

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -6,19 +6,17 @@ import routes from '../../client/src/routes'
 import cons from 'consolidate'
 import loopback from 'loopback'
 module.exports = function routes(app) {
+  
   app.engine("html", cons.handlebars);
   app.set("view engine", "html");
   app.set("views", "public/views");
   app.use(loopback.static("client/build"));
-  let bootstrap = {};
+
   app.get("/", function (req, res) {
     let HomeFactory = React.createFactory(Home);
     const html = ReactDOMServer.renderToString(HomeFactory({}));
-    console.log('HTML:\n\n\n\n ' + html);
     res.render("index", {
       markup: html,
-      bootstrap: JSON.stringify(bootstrap)
     });
-    
   });
 }
