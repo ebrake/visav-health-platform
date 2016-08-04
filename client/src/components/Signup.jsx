7e23f7cc01fb8713c98dc214b5977ff3a2b5505a
Nolan Thorn <nolan.thorn@krisandbrake.com>
2016-08-04 13:28:27 -0700

    Can actually create a user by inputting email and password

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 
-const createUser = (arg) => {
-  fetch(
-    'http://localhost:4000/user/create', 
-    {
-      method: 'POST', 
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email: 'nolan.thorn@krisandbrake.com', password: 'testtest'}) 
-    }
-  )
-  .then(function(response){
-    console.log('ok');
-    console.log(response);
-  })
-}
-
 var Login = React.createClass({
   mixins: null,
   cursors: {
     list: ['list']
+  },
+  createUser: function() {
+    if (!this.state.email) return console.log('No email!');
+    if (!this.state.password) return console.log('No password!');
+
+    fetch(
+      'http://localhost:4000/user/create', 
+      {
+        method: 'POST', 
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: this.state.email, password: this.state.password }) 
+      }
+    )
+    .then(function(response){
+      console.log('ok');
+      console.log(response);
+    })
+  },
+  getInitialState: function() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  handleChange: function(key) {
+    return function(event) {
+      var state = {};
+      state[key] = event.target.value;
+      this.setState(state);
+    }.bind(this);
   },
   render: function () {
     return (
@@ -30,7 +42,9 @@ var Login = React.createClass({
           <h2>Welcome to React</h2>
         </div>
         <div className="App-body">
-          <button className="fb-login-button" onClick={createUser}><span>Create User</span></button>
+          <input className="account-text-field" placeholder="Email" value={this.state.email} onChange={this.handleChange('email')} />
+          <input className="account-text-field" placeholder="Password" value={this.state.password} onChange={this.handleChange('password')} />
+          <button className="fb-login-button" onClick={this.createUser}><span>Create User</span></button>
         </div>
       </div>
     );
