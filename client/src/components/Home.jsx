b4bc2d61505f5dcbb8d0e5e0e458c24e8512c177
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-08-04 11:11:53 -0700

    localization library added with simple example on home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
-
+import LocalizedStrings from 'react-localization';
+import SegmentedControl from 'react-segmented-control'
+var language = "en";//default to english
+let strings = new LocalizedStrings({
+  en:{
+   welcome:"Welcome to Dillinger!",
+   english:"English",
+   french:"French"
+  },
+  fr: {
+   welcome:"Bienvenue à Dillinger!",
+   english:"Anglais",
+   french:"Français"
+  }
+});
 var Home = React.createClass({
   mixins: null,
   cursors: {
     list: ['list']
   },
+  updateLanguage: function(language) {
+    strings.setLanguage(language);
+    this.setState({});
+  },
   render: function () {
-
+    
     return (
       <div className="App">
         <div className="App-header header-class">
-          <h2>Welcome to React</h2>
+          <h2>{strings.welcome}</h2>
         </div>
+        <SegmentedControl 
+          onChange={this.updateLanguage} 
+          value={this.language}
+          name="language">
+          <span value="en">{strings.english}</span>
+          <span value="fr">{strings.french}</span>
+        </SegmentedControl>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
