3dd0c8edd3654d08c37bc9583ac315c422652829
Nolan Thorn <nolan.thorn@krisandbrake.com>
2016-08-09 15:20:44 -0700

    Fixed HealthData model, shows up in explorer, exposes 'create' POST function

--- common/models/health-data.js
+++ common/models/health-data.js
@@ -1,3 +1,17 @@
 module.exports = function(Healthdata) {
+  Healthdata.receiveData = function(req, cb) {
+    console.log('Healthdata received POST request...');
 
+    cb(null, null);
+  }
+
+  Healthdata.remoteMethod(
+    "receiveData",
+    {
+      accepts: [
+        { arg: 'data', type: 'object', http: { source: 'req' } }
+      ],
+      http: { path: '/create', verb: 'post' }
+    }
+  )
 };
