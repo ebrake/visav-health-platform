dd93e71cd8f9fe39db1fe52fe0b2f91037ce36a2
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-08-09 12:20:42 -0700

    reinstate server side rendering

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Router from "react-router";
 import ReactDOMServer from 'react-dom/server'
-
+import Charts from '../../client/src/components/pages/Charts.jsx'
 var System = require('es6-module-loader').System;
 
 const clientDir = (process.env.NODE_ENV=="production" ? "client-dist" : "client")
+
 
 System.import('../../'+clientDir+'/src/components/Home.jsx').then(function(m) {
 
@@ -22,9 +23,29 @@ import cons from 'consolidate'
 import loopback from 'loopback'
 
 module.exports = function routes(app) {
-  
   app.engine("html", cons.handlebars);
   app.set("view engine", "html");
   app.set("views", "public/views");
   app.use(loopback.static(clientDir+"/build"));
+
+  let bootstrap = {};
+  app.get("/", function (req, res) {
+    let HomeFactory = React.createFactory(Home);
+    const html = ReactDOMServer.renderToString(HomeFactory({}));
+    res.render("index", {
+      markup: html,
+      clientDir: 'client',
+      bootstrap: JSON.stringify(bootstrap)
+    });
+  });
+
+  app.get("/charts", function (req, res) {
+    let ChartFactory = React.createFactory(Charts);
+    const html = ReactDOMServer.renderToString(ChartFactory({}));
+    res.render("index", {
+      markup: html,
+      clientDir: 'client',
+      bootstrap: JSON.stringify(bootstrap)
+    });
+  });
 }
