eac88c2145823282bea54de9b6dcaa35defbdd0e
E Brake <e.brake@krisandbrake.com>
2016-08-11 16:37:46 -0700

    Added SystemJS dynamic module loading for production environment

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -3,17 +3,21 @@ import Router from "react-router";
 import ReactDOMServer from 'react-dom/server'
 import exphbs from 'express-handlebars'
 import loopback from 'loopback'
-import Charts from '../../client/src/components/pages/Charts.jsx'
-import Login from '../../client/src/components/pages/Login.jsx'
+import System from 'systemjs'
 
 const clientDir = (process.env.NODE_ENV=="production" ? "client-dist" : "client")
 
+// IMPORTANT: Do not delete. Needed for production
+SystemJS.import('../../'+clientDir+'/src/components/pages/Charts.jsx').then(function (_) {
+});
+SystemJS.import('../../'+clientDir+'/src/components/pages/Login.jsx').then(function (_) {
+});
 
 module.exports = function routes(app) {
 
   app.engine('handlebars', exphbs({extname: '.html'}));
   app.set('view engine', 'handlebars');
-  
+
   app.set("views", "public/views");
   app.use(loopback.static(clientDir+"/build"));
 
