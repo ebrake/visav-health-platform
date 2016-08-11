49fd0bff6182a9ace9079277c939cba80f66d100
flying-dragon <michael_wangql007@gmail.com>
2016-08-10 22:22:16 -0700

    try to manipulate DOM.

--- client/__tests__/src/components/Home-test.js
+++ client/__tests__/src/components/Home-test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 
+import SegmentedControl from 'react-segmented-control';
+
 //the module to test.
 var Home = require('../../../src/components/Home.js');
 
@@ -14,8 +16,25 @@ describe('home', function () {
     // by default, it should have french welcom
     expect(h2.textContent)
         .toEqual("Bienvenue à Dillinger!");
+
+    var seg_ctrl = TestUtils.findRenderedComponentWithType(
+    	home, SegmentedControl);
+    var seg_spans = TestUtils.scryRenderedDOMComponentsWithTag(
+    	seg_ctrl, 'span');
+    var span_en = null;
+    for(var i=0; i< seg_spans.length; i++){
+    	var seg_span = seg_spans[i];
+    	if(seg_span.value == 'en'){
+    		span_en = seg_span;
+    		console.log("Found ENNNNNNNNN!!!!!!");
+    		break;
+    	}
+    }
+    expect(seg_spans.length).toEqual(2);
+    expect(span_en).toEqual(null);
+    //expect(seg_ctrl.value).toEqual('en');
     
     // now let's switch to English....
-    
+
   });
 });
