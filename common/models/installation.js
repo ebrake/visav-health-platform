bd3ce8c39fea523dd6e04e73a45af9a90761068c
E Brake <e.brake@krisandbrake.com>
2016-08-18 14:28:10 -0700

    Created a route for registering devices for push notifications

---
+++ common/models/installation.js
@@ -0,0 +1,45 @@
+module.exports = function(Installation) {
+
+  Installation.remoteCreate = function(req, cb) {
+    if (!req.body.deviceToken) return cb(null, { error: new Error('No deviceToken!'), type: 'deviceToken', status: 'error' });
+    if (!req.body.deviceType) return cb(null, { error: new Error('No deviceType!'), type: 'deviceType', status: 'error' });
+    if (!req.body.appId) return cb(null, { error: new Error('No appId!'), type: 'appId', status: 'error' });
+
+    if (!req.user) {
+      return cb(null, { status: 'failure', message: 'Anonymous request (no user to attach to)' });
+    } 
+    var person = req.user;
+
+    Installation.create({
+        appId: req.body.appId,
+        userId: person.id,
+        deviceToken: req.body.deviceToken,
+        deviceType: req.body.deviceType,
+        created: new Date(),
+        modified: new Date(),
+        status: 'Active'
+    }, function (err, result) {
+        if (err) {
+          return cb(null, { error: err, type: 'signup', status: 'error' });
+        }
+        console.log('Registration record is created: ', result);
+        return cb(null, result);
+    });
+
+  }
+
+  Installation.remoteMethod(
+    "remoteCreate",
+    {
+      accepts: [
+        { arg: 'req', type: 'object', http: { source: 'req' } }
+      ],
+      http: { path: '/create', verb: 'post' },
+      returns: { arg: 'installation', type: 'object' },
+      description: "Accepts a registration for push notifications (deviceToken, deviceType, appID, person), returns the created push notification device installation"
+    }
+  );
+
+  return Installation;
+
+};
