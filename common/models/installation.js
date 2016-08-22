17f3b9299c1198f1842f1b897feb3b0b7bb5b7fc
E Brake <e.brake@krisandbrake.com>
2016-08-22 11:38:31 -0700

    Installation model overrides base schema; PUT sets userId to 1 (demo user)

--- common/models/installation.js
+++ common/models/installation.js
@@ -1,45 +1,16 @@
-module.exports = function(Installation) {
 
-  Installation.remoteCreate = function(req, cb) {
-    if (!req.body.deviceToken) return cb(null, { error: new Error('No deviceToken!'), type: 'deviceToken', status: 'error' });
-    if (!req.body.deviceType) return cb(null, { error: new Error('No deviceType!'), type: 'deviceType', status: 'error' });
-    if (!req.body.appId) return cb(null, { error: new Error('No appId!'), type: 'appId', status: 'error' });
+module.exports = function(installation) {
 
-    if (!req.user) {
-      return cb(null, { status: 'failure', message: 'Anonymous request (no user to attach to)' });
-    } 
-    var person = req.user;
+  installation.beforeRemote('replaceOrCreate',function(ctx, modelInstance, next){
 
-    Installation.create({
-        appId: req.body.appId,
-        deviceToken: req.body.deviceToken,
-        deviceType: req.body.deviceType,
-        created: new Date(),
-        modified: new Date(),
-        person: person.id,
-        status: 'Active'
-    }, function (err, result) {
-        if (err) {
-          return cb(null, { error: err, type: 'signup', status: 'error' });
-        }
-        console.log('Registration record is created: ', result);
-        return cb(null, result);
-    });
+    // Override userId with reference to user.
+    // Demo user is 1
 
-  }
+    // TODO: Link to actual user through request!
 
-  Installation.remoteMethod(
-    "remoteCreate",
-    {
-      accepts: [
-        { arg: 'req', type: 'object', http: { source: 'req' } }
-      ],
-      http: { path: '/create', verb: 'post' },
-      returns: { arg: 'installation', type: 'object' },
-      description: "Accepts a registration for push notifications (deviceToken, deviceType, appID, person), returns the created push notification device installation"
-    }
-  );
+    ctx.req.body.userId = 1;
 
-  return Installation;
+    next();
+  });
 
-};
+}
