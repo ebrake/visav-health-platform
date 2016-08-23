c9931be4e6a4bf99e16ae94c21cd889e7263ce1f
E Brake <e.brake@krisandbrake.com>
2016-08-23 09:11:52 -0700

    Reverted installation to use base schema's property "userId" instead of person; commented-out "ctx.req.params.id" as it was always null (for me!)

--- common/models/installation.js
+++ common/models/installation.js
@@ -3,8 +3,13 @@ module.exports = function(Installation) {
 
   Installation.beforeRemote('replaceOrCreate',function(ctx, modelInstance, next){
 
-    var id = ctx.req.params.id || null;
-    ctx.req.body.person = id;
+    //this is null for me...
+    //var id = ctx.req.params.id || null;
+    //ctx.req.body.userId = id;
+
+    // TODO: Link to user object, referenced through AUTH HTTP token.
+
+    ctx.req.body.userId = 1;
     if (ctx.req.params.deviceType=='iOS') {
       ctx.req.body.deviceType = 'ios';
     }
