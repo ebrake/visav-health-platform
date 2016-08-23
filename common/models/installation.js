a23831d63db3bc65965d1745261ab0892e56ffd6
E Brake <e.brake@krisandbrake.com>
2016-08-23 10:12:22 -0700

    Changed APN registration route to PUT /installation/register for supporting UPSERT

--- common/models/installation.js
+++ common/models/installation.js
@@ -1,20 +1,42 @@
 
 module.exports = function(Installation) {
 
-  Installation.beforeRemote('replaceOrCreate',function(ctx, modelInstance, next){
+  Installation.register = function(req, cb) {
 
     //this is null for me...
     //var id = ctx.req.params.id || null;
     //ctx.req.body.userId = id;
 
+    var userId = 1;
     // TODO: Link to user object, referenced through AUTH HTTP token.
 
-    ctx.req.body.userId = 1;
-    if (ctx.req.params.deviceType=='iOS') {
-      ctx.req.body.deviceType = 'ios';
+    var deviceType = req.body.deviceType;
+
+    var filter = {
+      where: {
+        deviceType: deviceType,
+        userId: userId
+      }
     }
-    
-    next();
-  });
+
+    req.body.userId = userId;
+
+    Installation.findOrCreate(filter, req.body, function(err, install, created) {
+      return cb(err,install);
+    });
+
+  }
+
+  Installation.remoteMethod(
+    "register",
+    {
+      accepts: [
+        { arg: 'req', type: 'object', http: { source: 'req' } }
+      ],
+      http: { path: '/register', verb: 'put' },
+      returns: { arg: 'installation', type: 'object' },
+      description: "Upserts an installation"
+    }
+  );
 
 }
