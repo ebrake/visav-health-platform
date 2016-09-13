6776cb148256b9fb954dc3d5a03199564ba7aa63
E Brake <e.brake@krisandbrake.com>
2016-09-13 11:42:32 -0700

    Assign unique IDs to each signal event

--- client/src/alt/stores/TelesessionStore.js
+++ client/src/alt/stores/TelesessionStore.js
@@ -23,6 +23,16 @@ class TelesessionStore {
 
   handleBroadcastChat(event) {
     console.log("Chat event sent from connection " + event.from.id + " :");
+    /** Assign a unique id to each event
+     * (Useful in React; Each child in an array or iterator should have a unique "key" prop)
+      {@link https://facebook.github.io/react/docs/multiple-components.html#dynamic-children|More Information}
+    */
+    if (!event.id) {
+      event.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      });
+    }
     console.log(event);
     this.chatEvents ? this.chatEvents.push(event) : this.chatEvents = [event];
   }
