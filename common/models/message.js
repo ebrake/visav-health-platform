f9088d9d0b358c9a5d7f81757e38d9475f115fba
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-09-13 15:01:52 -0700

    add plivo src number to env

--- common/models/message.js
+++ common/models/message.js
@@ -77,7 +77,7 @@ module.exports = function(Message) {
       authToken: process.env.PLIVO_AUTH_TOKEN
     });
     var plivoParams = {
-      'src': 'ALPHA-ID', // Sender's phone number with country code
+      'src': process.env.PLIVO_SRC_NUMBER, // Sender's phone number with country code
       'dst' : recipient.phone || 12508883312, // Receiver's phone Number with country code
       'text' : contentString || 'Hi, this is a test message from Visav',
     };
