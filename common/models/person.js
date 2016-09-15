f3c52ad907cdfc45373859095b87a2d19f3170c8
Nolan Thorn <nolan.thorn@krisandbrake.com>
2016-09-15 15:13:37 -0700

    Person.js now sends 200 as response code on every request that doesn't fail outside of our control, response always is data, always has status, tested and verified to work

--- common/models/person.js
+++ common/models/person.js
@@ -6,25 +6,28 @@ var path = require('path');
 
 module.exports = function(Person) {
 
-  Person.signup = function(req, cb) {
+  Person.signup = function(req, email, password, organizationName, cb) {
     var err;
+
     if(!req.body.email){
       err = new Error('Valid email required on req.body.email');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message, error: err });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_EMAIL';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
+
     if(!req.body.password){
       err = new Error('Valid password required on req.body.password');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message, error: err });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_PASSWORD';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
+
     if(!req.body.organizationName){
       err = new Error('Valid organization required on req.body.organizationName');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message, error: err });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_ORGANIZATIONNAME';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
 
     var orgFilter = { name: req.body.organizationName };
@@ -34,18 +37,21 @@ module.exports = function(Person) {
       if (queryResult[0]) {
         err = new Error('A person with this email has already been created');
         err.statusCode = 422;
-        err.code = 'PERSON_CREATE_FAILED_INVALID_REQUIREMENTS';
+        err.code = 'PERSON_CREATE_FAILED_INVALID_REQUIREMENT_DUPLICATE_EMAIL';
         throw err;
-      } else if (queryResult[1]) {
+      } 
+      else if (queryResult[1]) {
         err = new Error('An organization with this name has already been created');
         err.statusCode = 422;
-        err.code = 'PERSON_CREATE_FAILED_INVALID_REQUIREMENTS';
+        err.code = 'PERSON_CREATE_FAILED_INVALID_REQUIREMENT_DUPLICATE_ORGANIZATIONNAME';
         throw err;
-      } else {
+      } 
+      else {
         var personData = {
           email: req.body.email.toLowerCase(),
           password: req.body.password
         };
+
         return req.app.models.Organization.create(orgFilter)
         .then(function(createdOrganization){
           console.log('Organization created: '+createdOrganization.name);
@@ -55,11 +61,26 @@ module.exports = function(Person) {
       }
     })
     .then(function(data){
-      return cb( null, {status: 'success', user: data.user, organization: data.organization });
+      return cb(null, {status: 'success', user: data.user, organization: data.organization });
     }, function(err){
-      return cb(err, { status: 'failure', message: err.message, error: err });
+      return cb(null, { status: 'failure', message: err.message, error: err });
     })
   }
+
+  Person.remoteMethod(
+    "signup",
+    {
+      accepts: [
+        { arg: 'req', type: 'object', http: { source: 'req' } },
+        { arg: 'email', type: 'string' },
+        { arg: 'password', type: 'string' },
+        { arg: 'organizationName', type: 'string' }
+      ],
+      http: { path: '/signup', verb: 'post' },
+      returns: { arg: 'data', type: 'object' },
+      description: "Accepts a new user's email and password, returns the created user"
+    }
+  );
 
   //send verification email after registration
   Person.afterRemote('remoteCreate', function(context, createdObject, next) {
@@ -84,20 +105,21 @@ module.exports = function(Person) {
 
   });
 
-  Person.signin = function(req, cb){
+  Person.signin = function(req, email, password, cb){
     var err;
     if(!req.body.email){
       err = new Error('Valid email required on req.body.email');
       err.statusCode = 417;
-      err.code = 'PERSON_SIGNIN_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message });
+      err.code = 'PERSON_SIGNIN_FAILED_MISSING_REQUIREMENT_EMAIL';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
     if(!req.body.password){
       err = new Error('Valid password required on req.body.password');
       err.statusCode = 417;
-      err.code = 'PERSON_SIGNIN_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message });
+      err.code = 'PERSON_SIGNIN_FAILED_MISSING_REQUIREMENT_PASSWORD';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
+
     return Promise.all([
       Person.login({
         email: req.body.email.toLowerCase(),
@@ -108,38 +130,51 @@ module.exports = function(Person) {
       })
     ])
     .then(function(data){
-      console.log('Signed in user '+data[1].email);
-      return cb(null, { status: 'success', token: data[0], user: data[1] })
+      var message = 'Signed in user with email '+data[1].email
+      console.log(message);
+      return cb(null, { status: 'success', token: data[0], user: data[1], message: message })
     })
     .catch(function(err){
-      console.log('Issue signing in user:');
-      console.log(err);
-      return cb(err, { status: 'failure', message: err.message });
+      console.log('Issue signing in user: '+err.message);
+      return cb(null, { status: 'failure', message: err.message, error: err });
     })
   }
 
-  Person.invite = function(req, cb) {
+  Person.remoteMethod(
+    "signin",
+    {
+      accepts: [
+        { arg: 'req', type: 'object', http: { source: 'req' } },
+        { arg: 'email', type: 'string' },
+        { arg: 'password', type: 'string' }
+      ],
+      http: { path: '/signin', verb: 'post' },
+      returns: { arg: 'data', type: 'object' },
+      description: "Accepts a user's email and password, returns an access token"
+    }
+  );
+
+  Person.invite = function(req, email, role, cb) {
     var err;
-    if (!req.user.organization)
-      return cb(null, { error: new Error('No organization associated with user!'), type: 'organization', status: 'error' });
     if(!req.body.email){
       err = new Error('Valid email required on req.body.email');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_EMAIL';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
     if(!req.body.role){
       err = new Error('Valid Role required on req.body.role');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_ROLE';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
     if(!req.user.organization){
       err = new Error('No organization associated with user. Valid organization required on req.user.organization.');
       err.statusCode = 417;
-      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENTS';
-      return cb(err, { status: 'failure', message: err.message });
+      err.code = 'PERSON_CREATE_FAILED_MISSING_REQUIREMENT_NO_ORGANIZATION';
+      return cb(null, { status: 'failure', message: err.message, error: err });
     }
+
     var email = req.body.email.toLowerCase();
     var roleToAssign = req.body.role;
     var orgFilter = { id: req.user.organization.id };
@@ -149,13 +184,13 @@ module.exports = function(Person) {
       if (queryResult[0]) {
         err = new Error('A Person with this email has already been created.');
         err.statusCode = 422;
-        err.code = 'PERSON_INVITE_FAILED_INVALID_REQUIREMENTS';
+        err.code = 'PERSON_INVITE_FAILED_INVALID_REQUIREMENT_DUPLICATE_EMAIL';
         throw err;
       } else if (!queryResult[1]){
         //shouldn't be possible but we'll handle it anyways
         err = new Error('This organization does not exist.');
         err.statusCode = 422;
-        err.code = 'PERSON_INVITE_FAILED_INVALID_REQUIREMENTS';
+        err.code = 'PERSON_INVITE_FAILED_INVALID_REQUIREMENT_NO_ORGANIZATION';
         throw err;
       } else {
         var personData = {
@@ -172,43 +207,21 @@ module.exports = function(Person) {
     }, function(err){
       console.log('Error inviting person to organization:');
       console.log(err);
-      return cb(err, { status: 'failure', message: err.message });
+      return cb(null, { status: 'failure', message: err.message, error: err });
     })
   }
-
-  Person.remoteMethod(
-    "signin",
-    {
-      accepts: [
-        { arg: 'req', type: 'object', http: { source: 'req' } }
-      ],
-      http: { path: '/signin', verb: 'post' },
-      returns: { arg: 'data', type: 'object' },
-      description: "Accepts a user's email and password, returns an access token"
-    }
-  );
 
   Person.remoteMethod(
     "invite",
     {
       accepts: [
-        { arg: 'req', type: 'object', http: { source: 'req' } }
+        { arg: 'req', type: 'object', http: { source: 'req' } },
+        { arg: 'email', type: 'string' },
+        { arg: 'role', type: 'string' }
       ],
       http: { path: '/invite', verb: 'post' },
       returns: { arg: 'data', type: 'object' },
       description: "Takes in an email and role for a new user, creates their account and emails an invite to them."
-    }
-  );
-
-  Person.remoteMethod(
-    "signup",
-    {
-      accepts: [
-        { arg: 'req', type: 'object', http: { source: 'req' } }
-      ],
-      http: { path: '/signup', verb: 'post' },
-      returns: { arg: 'data', type: 'object' },
-      description: "Accepts a new user's email and password, returns the created user"
     }
   );
 }
