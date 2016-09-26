a2626e5d2d71cb7c12a22be3e786c2ef5354172c
Nolan Thorn <nolan.thorn@krisandbrake.com>
2016-09-26 15:47:12 -0700

    Updated colours in CSS, JS up next

--- client/src/components/utils/colors.js
+++ client/src/components/utils/colors.js
@@ -4,20 +4,21 @@
 
 var brightBlue = '114, 153, 204'
   , lightBlue = '150, 156, 180'
-  , mediumBlue = '60, 62, 74'
-  , darkBlue = '43, 46, 53'
   , purple = '167, 146, 224'
-  , darkestGrey = '17, 17, 17'
-  , darkGrey = '37, 37, 37'
   , mediumDarkGrey = '69, 69, 69'
-  , mediumLightGrey = '119, 119, 119'
   , lightGrey = '170, 170, 170'
   , lightestGrey = '221, 221, 221'
   , orange = '237, 165, 57'
   , red = '244, 87, 87'
   , green = '227, 227, 4'
   , white = '255, 255, 255'
-  , graphColors = [brightBlue, purple, orange, red, green]
+  , graphColors = [
+      brightBlue,
+      purple,
+      orange,
+      red,
+      green
+    ]
   , fontColors = {
       'dark': mediumDarkGrey,
       'medium': lightGrey,
