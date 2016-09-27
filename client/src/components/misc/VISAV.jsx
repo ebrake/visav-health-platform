6230b225de21a9fe295dbbf59e7450c2589ce722
Ethan Vaughan <ethan.vaughan@krisandbrake.com>
2016-09-27 12:30:39 -0700

    re-entered visav logo

--- client/src/components/misc/VISAV.jsx
+++ client/src/components/misc/VISAV.jsx
@@ -9,11 +9,11 @@ class VISAV extends React.Component {
   render () {
     return (
       <div className="visav-logo title">
-        <div className="logo-letter-container dark-blue"><div className="logo-letter">V</div></div>
-        <div className="logo-letter-container light-blue"><div className="logo-letter">I</div></div>
-        <div className="logo-letter-container purple"><div className="logo-letter">S</div></div>
-        <div className="logo-letter-container orange"><div className="logo-letter">A</div></div>
-        <div className="logo-letter-container red"><div className="logo-letter">V</div></div>
+        <div className="logo-letter-container dark-blue"><span className="logo-letter">V</span></div>
+        <div className="logo-letter-container light-blue"><span className="logo-letter">I</span></div>
+        <div className="logo-letter-container purple"><span className="logo-letter">S</span></div>
+        <div className="logo-letter-container orange"><span className="logo-letter">A</span></div>
+        <div className="logo-letter-container red"><span className="logo-letter">V</span></div>
       </div>
     );
   }
